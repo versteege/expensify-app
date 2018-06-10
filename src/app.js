79b753afc48b8dbc69aa80dd229bd6c3a36a2e2a
eversteeg@dexels.com <erik@dexels.com>
2018-06-10 15:53:41 +0200

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -15,13 +15,13 @@ import 'react-confirm-alert/src/react-confirm-alert.css';
 const store = configureStore();
 store.dispatch(setLocale({ locale: 'nl' }));
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);  
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);  
 
 // setTimeout(() => {
 //     store.dispatch(setTextFilter('rent'));
